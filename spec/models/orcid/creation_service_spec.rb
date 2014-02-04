f7ea8a4d6c35f82e9085e671013d8f86e938281e
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2014-02-04 13:49:43 -0500

    Namespacing Orcid interaction

--- spec/models/orcid/creation_service_spec.rb
+++ spec/models/orcid/creation_service_spec.rb
@@ -1,32 +1,11 @@
 require 'spec_helper'
 
-describe OrcidCreationService do
-  let(:payload) {
-    <<-XML
-    <?xml version="1.0" encoding="UTF-8"?>
-    <orcid-message
-    xmlns:xsi="http://www.orcid.org/ns/orcid https://raw.github.com/ORCID/ORCID-Source/master/orcid-model/src/main/resources/orcid-message-1.1.xsd"
-    xmlns="http://www.orcid.org/ns/orcid">
-    <message-version>1.1</message-version>
-    <orcid-profile>
-    <orcid-bio>
-    <personal-details>
-    <given-names>Albert</given-names>
-    <family-name>Einstein</family-name>
-    </personal-details>
-    <contact-details>
-    <email primary="true">a.einstein@orcid.org</email>
-    </contact-details>
-    </orcid-bio>
-    </orcid-profile>
-    </orcid-message>
-    XML
-  }
-
+describe Orcid::CreationService do
+  let(:payload) { %(<?xml version="1.0" encoding="UTF-8"?>) }
   let(:user) { double }
   let(:config) { {access_token: access_token, host: 'https://api.sandbox-1.orcid.org'} }
   let(:access_token) { '6e43b7b9-7d78-4fee-baa4-76acee469b7d' }
-  subject { OrcidCreationService.new(config) }
+  subject { described_class.new(config) }
 
   context '#call' do
     let(:minted_orcid) { '0000-0001-8025-637X' }
