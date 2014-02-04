a1106ac079a3f8234b143f2724c281624b65b6ea
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2014-02-04 10:30:45 -0500

    Modifying User intitialization from session
    Given that we want to allow alternate authentications, modifying
    User.new_with_session to build an authentication if the session
    contains authentication information.

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,19 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable,
-         :omniauthable, :omniauth_providers => [:github]
+    :recoverable, :rememberable, :trackable, :validatable,
+    :omniauthable, :omniauth_providers => [:github]
+
+  has_many :authentications, dependent: :destroy
+
+  def self.new_with_session(params, session)
+    super.tap do |user|
+      if auth_data = session['devise.auth_data']
+        user.authentications.build(
+          provider: auth_data.fetch('provider'),
+          uid: auth_data.fetch('uid')
+        )
+      end
+    end
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,28 @@
 require 'spec_helper'
 
 describe User do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context '.new_with_session' do
+    let(:parameters) { {} }
+    let(:provider) { 'twitter' }
+    let(:uid) { '1234' }
+    subject { User.new_with_session(parameters, session) }
+    context 'with devise.auth_data' do
+      let(:session) {
+        {'devise.auth_data' => { 'provider' => provider, 'uid' => uid}}
+      }
+      its(:persisted?) { should be_false }
+      it 'should have a non-persisted authentication' do
+        expect(subject.authentications.size).to eq(1)
+        expect(subject.authentications.first).to_not be_persisted
+      end
+    end
+
+    context 'without devise.auth_data' do
+      let(:session) { {} }
+      its(:persisted?) { should be_false }
+      it 'should not build an associated authentication' do
+        expect(subject.authentications.size).to eq(0)
+      end
+    end
+  end
 end
