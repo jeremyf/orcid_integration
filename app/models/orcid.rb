16de6351395ad70c959a4fb72ea3adb7bcaae9f9
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2014-02-10 14:10:39 -0500

    Adding Orcid.profile_search_access_token
    We need a public search access token

--- app/models/orcid.rb
+++ app/models/orcid.rb
@@ -8,9 +8,16 @@ module Orcid
     object.run
   end
 
+  # @NOTE - The tokens may expire; This is presently note handled.
   def profile_creation_access_token(options = {})
     creation_service = options.fetch(:creation_service) { Orcid::ApplicationAccessTokenCreationService }
     cache[:profile_creation_access_token] ||= creation_service.call(scope: '/orcid-profile/create', grant_type:'client_credentials').fetch('access_token')
+  end
+
+  # @NOTE - The tokens may expire; This is presently note handled.
+  def profile_search_access_token(options = {})
+    creation_service = options.fetch(:creation_service) { Orcid::ApplicationAccessTokenCreationService }
+    cache[:profile_creation_access_token] ||= creation_service.call(scope: '/read-public', grant_type:'client_credentials').fetch('access_token')
   end
 
   def cache
--- spec/models/orcid_spec.rb
+++ spec/models/orcid_spec.rb
@@ -21,4 +21,16 @@ describe Orcid do
       expect(Orcid.profile_creation_access_token(creation_service: creation_service)).to eq(access_token)
     end
   end
+
+  context '.profile_search_access_token' do
+    let(:creation_service) { double('Service') }
+    let(:service_response) { { 'access_token' => access_token } }
+    let(:access_token) { double('Access Token') }
+    it 'should return the access_token' do
+      creation_service.should_receive(:call).
+        with(scope: '/read-public', grant_type:'client_credentials').
+        and_return(service_response)
+      expect(Orcid.profile_search_access_token(creation_service: creation_service)).to eq(access_token)
+    end
+  end
 end
