6aeb7063ad33dfdb8b825e35e7132fa0b4091842
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2014-02-10 21:31:02 -0500

    Enabling attribute setting for #new action
    I want to be able to allow the attributes to be set during the
    ProfileConnections#new action

--- app/controllers/orcid/profile_connections_controller.rb
+++ app/controllers/orcid/profile_connections_controller.rb
@@ -4,13 +4,27 @@ module Orcid
     before_filter :authenticate_user!
 
     def new
+      assign_attributes(new_profile_connection)
       respond_with(new_profile_connection)
     end
+
+    # def create
+    #   assign_attributes(new_profile_connection)
+    #   respond_with(new_profile_connection)
+    # end
 
     protected
 
     attr_reader :profile_connection
     helper_method :profile_connection
+
+    def assign_attributes(profile_connection)
+      profile_connection.attributes = profile_connection_params
+    end
+
+    def profile_connection_params
+      params[:profile_connection] || {}
+    end
 
     def new_profile_connection
       @profile_connection ||= Orcid::ProfileConnection.new(params[:profile_connection])
