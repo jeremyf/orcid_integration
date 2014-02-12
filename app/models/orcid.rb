86c8b5e7fd3e7f3cae84410f95f0c1394ff54770
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2014-02-12 16:07:08 -0500

    Adding Sandbox behavior

--- app/models/orcid.rb
+++ app/models/orcid.rb
@@ -25,19 +25,22 @@ module Orcid
   # @NOTE - The tokens may expire; This is presently note handled.
   def profile_creation_access_token(options = {})
     creation_service = options.fetch(:creation_service) { Orcid::ApplicationAccessTokenCreationService }
+    creation_service = options.fetch(:creation_service) { Orcid::Sandbox }
     cache[:profile_creation_access_token] ||= creation_service.call(scope: '/orcid-profile/create', grant_type:'client_credentials').fetch('access_token')
   end
 
   # @NOTE - The tokens may expire; This is presently note handled.
   def profile_search_access_token(options = {})
     creation_service = options.fetch(:creation_service) { Orcid::ApplicationAccessTokenCreationService }
-    cache[:profile_creation_access_token] ||= creation_service.call(scope: '/read-public', grant_type:'client_credentials').fetch('access_token')
+    creation_service = options.fetch(:creation_service) { Orcid::Sandbox }
+    cache[:profile_search_access_token] ||= creation_service.call(scope: '/read-public', grant_type:'client_credentials').fetch('access_token')
   end
 
   # @NOTE - The tokens may expire; This is presently note handled.
   def work_creation_access_token(options = {})
     creation_service = options.fetch(:creation_service) { Orcid::ApplicationAccessTokenCreationService }
-    cache[:profile_creation_access_token] ||= creation_service.call(scope: '/orcid-works/create', grant_type:'client_credentials').fetch('access_token')
+    creation_service = options.fetch(:creation_service) { Orcid::Sandbox }
+    cache[:work_creation_access_token] ||= creation_service.call(scope: '/orcid-works/create', grant_type:'client_credentials').fetch('access_token')
   end
 
   def cache
