e9018ee5774b585ea7b83f13357b26fabd016d97
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2014-02-14 09:38:33 -0500

    Refactoring to use OAuth2 pattern/classes
    Instead of relying on RestClient and configuration, utilize an OAuth
    token.

--- spec/services/orcid/append_new_work_service_spec.rb
+++ spec/services/orcid/append_new_work_service_spec.rb
@@ -3,36 +3,20 @@ require 'spec_helper'
 module Orcid
   describe AppendNewWorkService do
     let(:payload) { %(<?xml version="1.0" encoding="UTF-8"?>) }
-    let(:user) { double }
-    let(:config) { {access_token: access_token, host: 'https://api.sandbox-1.orcid.org'} }
-    let(:access_token) { '6e43b7b9-7d78-4fee-baa4-76acee469b7d' }
+    let(:config) { {token: token, headers: request_headers } }
+    let(:token) { double("Token") }
     let(:orcid_profile_id) { '0000-0003-1495-7122' }
-
-    let(:request_headers) {
-      {
-        'Content-Type' => 'application/orcid+xml',
-        'Accept' => 'application/xml',
-        'Authorization' => "Bearer #{access_token}"
-      }
-    }
+    let(:request_headers) { { 'Content-Type' => 'application/orcid+xml', 'Accept' => 'application/xml' } }
+    let(:response) { double("Response") }
 
     subject { described_class.new(config) }
-    before(:each) do
-      stub_request(:post, File.join(config[:host], "v1.1", orcid_profile_id, 'orcid-works/')).
-        with(body: payload, headers: request_headers).
-        to_return(status: 200)
-    end
-
-    context '#call' do
-      context 'with valid data' do
-        it 'should return the minted orcid' do
-          expect(subject.call(orcid_profile_id, payload)).to eq(true)
-        end
-      end
-    end
 
     context '.call' do
       it 'instantiates and calls underlying instance' do
+        token.should_receive(:post).
+          with("v1.1/#{orcid_profile_id}/orcid-works/", body: payload, headers: request_headers).
+          and_return(response)
+
         expect(described_class.call(orcid_profile_id, payload, config)).to eq(true)
       end
     end
