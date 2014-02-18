0bf46808a26e499b352166aa0cfa090d02824965
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2014-02-18 12:12:41 -0500

    Extracting Profile mapper configuration up to init
    Instead of extracting the mapper for the profile at call time, instead
    extract at init time.

--- app/models/orcid/profile.rb
+++ app/models/orcid/profile.rb
@@ -1,25 +1,26 @@
 module Orcid
   class Profile
 
-    attr_reader :orcid_profile_id
-    def initialize(orcid_profile_id)
+    attr_reader :orcid_profile_id, :mapper
+    private :mapper
+    def initialize(orcid_profile_id, config = {})
       @orcid_profile_id = orcid_profile_id
+      @mapper = config.fetch(:mapper) { ::Mappy }
     end
 
-    def append_new_work(work, config = {})
-      remote_service = config.fetch(:remote_service) { Orcid::AppendNewWorkService }
-      orcid_work = normalize_work(work, config)
+    def append_new_work(work, options = {})
+      remote_service = options.fetch(:remote_service) { Orcid::AppendNewWorkService }
+      orcid_work = normalize_work(work)
       remote_service.call(orcid_profile_id, orcid_work.to_xml)
     end
 
     protected
 
-    def normalize_work(work,config)
+    def normalize_work(work)
       if work.is_a?(Orcid::Work)
-        orcid_work = work
+        work
       else
-        mapper = config.fetch(:mapper) { ::Mappy }
-        orcid_work = mapper.map(work, target: 'orcid/work')
+        mapper.map(work, target: 'orcid/work')
       end
     end
 
--- spec/models/orcid/profile_spec.rb
+++ spec/models/orcid/profile_spec.rb
@@ -3,19 +3,19 @@ require 'spec_helper'
 module Orcid
   describe Profile do
     let(:orcid_profile_id) { '0001-0002-0003-0004' }
-    subject { described_class.new(orcid_profile_id) }
+    let(:remote_service) { double('Service') }
+    let(:mapper) { double("Mapper") }
+    subject { described_class.new(orcid_profile_id, mapper: mapper) }
 
     context '#append_new_work' do
       let(:non_orcid_work) { double("A non-ORCID Work") }
       let(:orcid_work) { double("Orcid::Work", to_xml: 'Look I am XML') }
-      let(:remote_service) { double('Service') }
-      let(:mapper) { double("Mapper") }
 
       it 'should transform the input work to xml and deliver to the remote_service' do
         remote_service.should_receive(:call).with(orcid_profile_id, orcid_work.to_xml)
         mapper.should_receive(:map).with(non_orcid_work, target: 'orcid/work').and_return(orcid_work)
 
-        subject.append_new_work(non_orcid_work, remote_service: remote_service, mapper: mapper)
+        subject.append_new_work(non_orcid_work, remote_service: remote_service)
       end
     end
   end
