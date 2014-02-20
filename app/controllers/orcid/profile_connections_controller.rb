864cd73489fcdeb6ec59c2ffc1f086b878d895d9
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2014-02-20 12:35:03 -0500

    Conditionally redirecting from profile connections
    If someone already has an associated Orcid::Profile, I don't want them
    to connect to another profile.

--- app/controllers/orcid/profile_connections_controller.rb
+++ app/controllers/orcid/profile_connections_controller.rb
@@ -8,11 +8,13 @@ module Orcid
     end
 
     def new
+      return false if redirecting_because_user_already_has_a_connected_orcid_profile
       assign_attributes(new_profile_connection)
       respond_with(new_profile_connection)
     end
 
     def create
+      return false if redirecting_because_user_already_has_a_connected_orcid_profile
       assign_attributes(new_profile_connection)
       create_profile_connection(new_profile_connection)
       respond_with(new_profile_connection)
@@ -22,6 +24,16 @@ module Orcid
 
     attr_reader :profile_connection
     helper_method :profile_connection
+
+    def redirecting_because_user_already_has_a_connected_orcid_profile
+      if orcid_profile = Orcid.profile_for(current_user)
+        flash[:notice] = I18n.t("orcid.requests.messages.previously_connected_profile", orcid_profile_id: orcid_profile.orcid_profile_id)
+        redirect_to root_path
+        return true
+      else
+        return false
+      end
+    end
 
     def assign_attributes(profile_connection)
       profile_connection.attributes = profile_connection_params
--- spec/controllers/orcid/profile_connections_controller_spec.rb
+++ spec/controllers/orcid/profile_connections_controller_spec.rb
@@ -10,7 +10,28 @@ module Orcid
         end
       end
     end
+
+    def self.it_redirects_if_user_has_previously_connected_to_orcid_profile(method, action)
+      context 'user has existing orcid_profile' do
+        it 'should redirect to home_path' do
+          sign_in(user)
+          orcid_profile = double("Orcid::Profile", orcid_profile_id: '1234-5678-0001-0002')
+          Orcid.should_receive(:profile_for).with(user).and_return(orcid_profile)
+
+          send(method, action)
+
+          expect(response).to redirect_to(root_path)
+          expect(flash[:notice]).to eq(
+            I18n.t("orcid.requests.messages.previously_connected_profile", orcid_profile_id: orcid_profile.orcid_profile_id)
+          )
+        end
+      end
+    end
+
     let(:user) { mock_model('User') }
+    before(:each) do
+      Orcid.stub(:profile_for).and_return(nil)
+    end
 
     context 'GET #index' do
       it_prompts_unauthenticated_users_for_signin(:get, :index)
@@ -18,6 +39,7 @@ module Orcid
 
     context 'GET #new' do
       it_prompts_unauthenticated_users_for_signin(:get, :new)
+      it_redirects_if_user_has_previously_connected_to_orcid_profile(:get, :new)
 
       context 'authenticated and authorized user' do
         before { sign_in(user) }
@@ -33,6 +55,7 @@ module Orcid
 
     context 'POST #create' do
       it_prompts_unauthenticated_users_for_signin(:post, :create)
+      it_redirects_if_user_has_previously_connected_to_orcid_profile(:post, :create)
 
       context 'authenticated and authorized user' do
         let(:orcid_profile_id) {'0000-0001-8025-637X'}
