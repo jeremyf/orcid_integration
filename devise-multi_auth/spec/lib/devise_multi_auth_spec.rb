61bd8ee54d70209461c74433a708baff005ed3a2
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2014-02-20 20:26:05 -0500

    Cleaning up duplicate name

--- devise-multi_auth/spec/lib/devise_multi_auth_spec.rb
+++ devise-multi_auth/spec/lib/devise_multi_auth_spec.rb
@@ -5,7 +5,7 @@ describe Devise::MultiAuth do
   context '.capture_successful_external_authentication' do
     let(:service) { double('Service') }
     let(:user) { double('User') }
-    let(:user) { double('Auth') }
+    let(:auth) { double('Auth') }
     it 'should delegate to the associated service' do
       service.should_receive(:call).with(user, auth)
       described_class.capture_successful_external_authentication(user, auth, service: service)
