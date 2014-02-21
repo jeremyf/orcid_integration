ee7d0e5bf0f3769b1368a7f76217c927e8a258e4
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2014-02-21 10:43:21 -0500

    Adding app/services autoload paths

--- orcid/lib/orcid/engine.rb
+++ orcid/lib/orcid/engine.rb
@@ -1,5 +1,13 @@
 module Orcid
   class Engine < ::Rails::Engine
     isolate_namespace Orcid
+
+    initializer 'orcid.initializers' do |app|
+      app.config.paths.add 'app/services', eager_load: true
+      app.config.autoload_paths += %W(
+        #{config.root}/app/services
+      )
+    end
+
   end
 end
